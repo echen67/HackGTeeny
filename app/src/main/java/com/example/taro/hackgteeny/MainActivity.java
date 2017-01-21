ac783b9694f5a333e85b145087cc3fdc30f669f8
Dorkly <bree_cakesx3@live.com>
2017-01-21 17:21:07 -0500

    Revert!

--- app/src/main/java/com/example/taro/hackgteeny/MainActivity.java
+++ app/src/main/java/com/example/taro/hackgteeny/MainActivity.java
@@ -9,9 +9,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-        private RecyclerView mRecyclerView;
-        private RecyclerView.Adapter mAdapter;
-        private RecyclerView.LayoutManager mLayoutManager;
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -20,15 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String[] names = {"Emily","Bryanna","Roy"};
         setContentView(R.layout.activity_main);
-        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mAdapter = new MyAdapter(names);
-        mRecyclerView.setAdapter(mAdapter);
-
 
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
@@ -45,6 +34,6 @@ public class MainActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("What it Do world ;o");
+        myRef.setValue("Hello, World!");
     }
 }
--- app/src/main/java/com/example/taro/hackgteeny/MyAdapter.java
+++ app/src/main/java/com/example/taro/hackgteeny/MyAdapter.java
@@ -7,18 +7,11 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 /**
-<<<<<<< HEAD
- * Created by Bree~ on 1/21/2017.
- */
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-    private String[] mDataset;
-=======
  * Created by Taro on 1/21/2017.
  */
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     public String[] mDataset;
->>>>>>> origin/master
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
@@ -26,24 +19,6 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
         public TextView mTextView;
-<<<<<<< HEAD
-
-        public ViewHolder(View v) {
-            super(v);
-            mTextView = (TextView) v.findViewById(R.id.my_recycler_view);
-            this.mTextView = mTextView;
-        }
-    }
-    // Provide a suitable constructor (depends on the kind of dataset)
-        public MyAdapter(String[] myDataset) {
-        mDataset = myDataset;
-    }
-    // Create new views (invoked by the layout manager)
-    @Override
-    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        // create a new view
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_textview, parent, false);
-=======
         public ViewHolder(View v) {
             super(v);
             mTextView = (TextView) v.findViewById(R.id.textview);
@@ -63,15 +38,11 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         // create a new view
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.mytextview, parent, false);
->>>>>>> origin/master
         // set the view's size, margins, paddings and layout parameters
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
     // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
@@ -86,11 +57,4 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     public int getItemCount() {
         return mDataset.length;
     }
-<<<<<<< HEAD
 }
-
-
-
-=======
-}
->>>>>>> origin/master
