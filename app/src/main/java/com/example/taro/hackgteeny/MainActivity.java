044c319ae86bb1af95e28fec0bef6c981eade973
Dorkly <bree_cakesx3@live.com>
2017-01-21 16:42:43 -0500

    Update

--- app/src/main/java/com/example/taro/hackgteeny/MainActivity.java
+++ app/src/main/java/com/example/taro/hackgteeny/MainActivity.java
@@ -2,21 +2,34 @@ package com.example.taro.hackgteeny;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+        private RecyclerView mRecyclerView;
+        private RecyclerView.Adapter mAdapter;
+        private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String[] names = {"Emily","Bryanna","Roy"};
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MyAdapter(names);
+        mRecyclerView.setAdapter(mAdapter);
+
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!");
+        myRef.setValue("What it Do world ;o");
     }
 }
