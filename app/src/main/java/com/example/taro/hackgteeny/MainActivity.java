5edf19eb4029b9c18e9d54e72911eca1b9f6e00c
echen67 <chenemily3@yahoo.com>
2017-01-21 16:40:33 -0500

    a lot of stuff

--- app/src/main/java/com/example/taro/hackgteeny/MainActivity.java
+++ app/src/main/java/com/example/taro/hackgteeny/MainActivity.java
@@ -2,16 +2,33 @@ package com.example.taro.hackgteeny;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        String[] myDataset = {"hi", "e", "b"};
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
