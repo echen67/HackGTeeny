aac9024d78f061e27e01b86dd2450cc86d8eccc6
echen67 <chenemily3@yahoo.com>
2017-01-21 20:34:07 -0500

    done

--- app/src/main/java/com/example/taro/hackgteeny/MainActivity.java
+++ app/src/main/java/com/example/taro/hackgteeny/MainActivity.java
@@ -1,23 +1,38 @@
 package com.example.taro.hackgteeny;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private MyAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    //public static String[] myDataset = {"hi", "e", "b"};
+    public ArrayList<String> myDataset = new ArrayList<>(5);
+    public static String EXTRA_MESSAGE = "bleh";
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
@@ -26,7 +41,9 @@ public class MainActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        String[] myDataset = {"hi", "e", "b"};
+        myDataset.add("hi");
+        myDataset.add("bye");
+        //String[] myDataset = {"hi", "e", "b"};
         mAdapter = new MyAdapter(myDataset);
         mRecyclerView.setAdapter(mAdapter);
 
@@ -35,5 +52,56 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");
+
+        final EditText text = (EditText) findViewById(R.id.editmessage);
+        final Button button = (Button) findViewById(R.id.buttonid);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mDatabase.push().child("title").setValue(text.getText().toString());
+                text.setText("");
+            }
+        });
+
+        mDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                //mAdapter.add2((String) dataSnapshot.child("title").getValue());
+                mAdapter.add((String) dataSnapshot.child("title").getValue());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                //adapter.remove((String) dataSnapshot.child("title").getValue());
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void sendMessage(View view) {
+        //EditText editText = (EditText) findViewById(R.id.editmessage);
+        //String message = editText.getText().toString();
+        //myDataset[0] = "hello?";
+        //myDataset.add("clicked!!");
+        //mAdapter = new MyAdapter(myDataset);
+        //mRecyclerView.setAdapter(mAdapter);
+        //Intent intent = new Intent(this, DisplayMessageActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.editmessage);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        //startActivity(intent);
     }
 }
--- app/src/main/java/com/example/taro/hackgteeny/MyAdapter.java
+++ app/src/main/java/com/example/taro/hackgteeny/MyAdapter.java
@@ -6,12 +6,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 /**
  * Created by Taro on 1/21/2017.
  */
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-    public String[] mDataset;
+    //public String[] mDataset;
+    public ArrayList<String> mDataset;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
@@ -27,9 +30,18 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public MyAdapter(String[] myDataset) {
+    //public MyAdapter(String[] myDataset) {
+        //mDataset = myDataset;
+    //}
+
+    public MyAdapter(ArrayList myDataset) {
         mDataset = myDataset;
     }
+
+    public void add(String str) {
+        mDataset.add(str);
+    }
+
 
     // Create new views (invoked by the layout manager)
     @Override
@@ -48,13 +60,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     public void onBindViewHolder(ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.mTextView.setText(mDataset[position]);
+        holder.mTextView.setText(mDataset.get(position));
 
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return mDataset.size();
     }
 }
